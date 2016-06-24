6ffd1f7aae2f60fbaacca32efbc51e8235dcf83f
40323145 <40323145@gm.nfu.edu.tw>
2016-06-24 16:40:18 +0800

    40323145 0624 16:40

--- myflaskapp.py
+++ myflaskapp.py
@@ -6,7 +6,7 @@ import os
 # init.py 為自行建立的起始物件
 import init
 
-
+import users.a.g8.ag8_40323145
 import users.a.g8.ag8_40323123_task2
 # 確定程式檔案所在目錄, 在 Windows 有最後的反斜線
 _curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -378,4 +378,4 @@ def send_file(path):
 if __name__ == "__main__":
     app.run()
 
-app.register_blueprint(users.a.g8.ag8_40323123_task2.ag8_test)
+app.register_blueprint(users.a.g8.ag8_40323145.ag8_40323145)
